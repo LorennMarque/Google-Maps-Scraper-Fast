048f4640d710c331f7a20056ccf04ba754dbf3fc
lorenzoMarquesini <lorenzomarquesini@gmail.com>
2024-02-19 11:34:29 -0300

    Privacy

--- create-db-file.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-from sqlalchemy import create_engine
-
-engine = create_engine('sqlite:///places_data.db', echo=True)
-
-pd.read_csv('places_data.csv').to_sql('locations', con=engine, if_exists='replace', index=False)
